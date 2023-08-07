bcce8a68d10aadca3e479a7c2af10c21456dcbad
Alex Paino <alphaprime@Alexs-MacBook-Pro.local>
2023-08-06 21:25:49 -0400

    lesson 7 080623

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,6 +2,13 @@
 
 pragma solidity ^0.8.18;
 
+// 4 types of tests
+
+// 1 - unit tests - testing a specific part of our code
+// 2 - integration tests - testing how different parts of our code work together
+// 3 - forked tests - testing how our smart contract works with other contracts in simulated real environment
+// 4 - staging tests - testing how our smart contract works with other contracts on a testnet
+
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
@@ -9,10 +16,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         // fundMe = new FundMe();
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -20,11 +32,23 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // expect revert
+        fundMe.fund(); // call fund @ value = 0
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
